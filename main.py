77270b72b1a228e730cf9a14a9372215246e1c44
JulianoCP <juliano-petini@hotmail.com>
2018-11-22 19:11:58 -0200

    Add more

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 class No:
-    def __init__(self):
-        self.dado = list()
+    def __init__(self,dado):
+        self.dado = dado
         self.listaNos = list()
-        self.ePalavra = False
+        self.ePalavra = True
 
     def __str__(self):
-        return "OLA "+self.dado
+        return self.dado
 
-    def addDado(self, novoDado):
-        self.dado = novoDado
+def addNo(root, entrada):
+    if entrada in root.dado:
+        print(entrada,'é prefixo de',root.dado)
+    elif root.dado in entrada:
+        print(root.dado, 'é prefixo de ',entrada)
+
+    # if entrada in i:
+    #     print(entrada,' prefixo de ',y)
+    #     for i in entrada:
+    #         k+=1
+    #     j=y[k:]
+    #     t=y[:k]
+    #     print(j,' e ',t)
+    # elif y in entrada:
+    #     print(y,' prefixo de ',entrada)
+    # else:
+    #     print('incompativeis')
 
 
 def main():
-    meuNo = No()
-    meuNo.addDado("Michel")
-    
+    root = No("recanto")
+    print(root)
+    addNo(root,"canto")
+
 
 if __name__ == "__main__":
     main()
