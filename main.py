a780183c0bc4a017081f6dfe85151c3fe7af8e7e
Luis Otavio <luiscapelari@alunos.utfpr.edu.br>
2018-11-24 15:33:42 -0200

    upgrade
    melhorias na inserção

--- main.py
+++ main.py
@@ -9,36 +9,55 @@ class No:
 
 def addNo(root, entrada):
     #percorrer as duas string para conferir compatibilidades
+    match=0
     for i in range(0,len(entrada)):
         if i<len(root.dado) :
             if entrada[i]!=root.dado[i] :
                 #se possuem diferenças entre as strings
                 break
+            else:
+                match+=1
         else:
             #se ultrapssou o tamanho da string dado em root
             break
 
     #mostrar a entrada , o root, ate que posiçao percorreu nas string, e qual atitude a ser tomada
-    print('entrada',entrada.upper(),'root',root.dado.upper(),'foi ate pos',i,'resultado:',end=' ')
+    print('entrada',entrada.upper(),'root'.rjust(14-len(entrada)),root.dado.upper(),'resultado:'.rjust(12),end=' ')
     #4 casos :
-    if i < len(root.dado) :
-        #primeiro caso: palavras diferentes
-        if i==0:
-            print('nao tem nada a ver, criar nó nulo')
-        #segundo caso: inserindo uma palavra que ja existe na arvore
-        elif entrada == root.dado:
-            print('nao precisa fazer nada, sao iguais')
+    #primeiro caso: inserindo uma palavra que ja existe na arvore
+    if entrada == root.dado:
+        print('nao precisa fazer nada, sao iguais'.capitalize(),entrada[:match].upper())
+        #precisa fazer só quando o atual ePalavra = False
+    elif match < len(root.dado) :
+        #segundo caso: palavras diferentes
+        if match==0:
+            print('nao tem nada a ver, criar nó nulo'.capitalize())
+            #new=No(none)
+            #new.ePalavra=False
+            #new.listaNos.append(root,entrada)
         #terceiro caso: prefixo em comum, podendo esse prefixo ser a entrada ou parte dela
         else:
-            print('criar nó a cima com palavra nova',entrada[:i].upper())
-
-    elif i == len(root.dado):
+            print('criar nó nulo a cima com palavra nova'.capitalize(),entrada[:match].upper())
+            #new=No(entrada[:match])
+            #other=No(entrada[match:]) #detalhe para match ser igual o tamanho da entrada
+            #other2=No(root[match:])
+            #new.listaNos.append(other,other2)
+    elif match == len(root.dado):
         #quarto caso: prefixo em comum, sendo esse prefixo o root
         if len(entrada) > len(root.dado):
-            print('inseir como filho de root a palavra',entrada[i:].upper())
+            print('inseir como filho de root a palavra'.capitalize(),entrada[match:].upper())
+            #recursao, precisa verificar na lista de filhos, se possui algum filho com o novo prefixo
+            #new=No(entrada[match:])
+            #root.listaNos.append(new)
     print()
 
-
+#base para busca recursiva
+#root ='can'
+#l=['oa','ta','ario','sao']
+#entr=input()
+#for i in l:
+#    if entr[0] in i:
+#        print(i)
 
 def main():
     root = No('canto')
@@ -54,7 +73,9 @@ def main():
     #mais exemplos
     addNo(root,'cantora')
     addNo(root,'cantoria')
+    addNo(root,'canta')
     addNo(root,'canarinho')
+    addNo(root,'corsinha')
 
 
 if __name__ == "__main__":
