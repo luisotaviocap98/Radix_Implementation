5a55be0be721d4586fe5a8c5d10b59828c1c01d1
Michel Gomes <michels@alunos.utfpr.edu.br>
2018-11-26 22:48:45 -0200

    Print Nodes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class No:
         self.dado = dado#reduzido
         self.listaNos = list()
         self.ePalavra = True
-        self.pOriginal = None
+        self.pOriginal = dado
 
 
 def addNo(root, entrada):
@@ -29,6 +29,7 @@ def addNo(root, entrada):
     if entrada == root.dado:
         if(root.ePalavra == False):
             root.ePalavra = True
+            root.pOriginal = palavra
         if(root.ePalavra == True):
             print('mudou ou ja era true',end= '     ')
         print('nao precisa fazer nada, sao iguais'.capitalize(),entrada[:match].upper())
@@ -44,44 +45,30 @@ def addNo(root, entrada):
             new.listaNos.append(root)
             new.listaNos.append(word)
             root = new
-            '''
-            for i in root.listaNos:
-                print(i.dado)
-                for j in i.listaNos:
-                    print(j.dado)
-            '''
+
         #terceiro caso: prefixo em comum, podendo esse prefixo ser a entrada ou parte dela
         else:
             print('criar nó nulo a cima com palavra nova'.capitalize(),entrada[:match].upper())
             new=No(entrada[:match])
             print('\nMATCH ',match,'LEN',len(entrada),'NEW',new.dado)
+            print('\ROOT ',root.dado)
             if match != len(entrada):   #menor
                 other=No(entrada[match:]) #detalhe para match ser igual o tamanho da entrada
                 new.listaNos.append(other)
 
-
-
-
-                # vai ter que ter recursão
-                print('11111',other.dado)
             other2=No(root.dado[match:])
+            other2.ePalavra = False
 
             for i in root.listaNos:
+                i.dado = i.dado.replace("\n", '')
                 other2.listaNos.append(i)
 
-            #    print('NoGravado ',i.dado)
-            print('22222',other2.dado)
-            #print()
-            #print('Tamanho ',len(other2.listaNos))
-            #print('Nós ',other2.listaNos[1])
 
-
-            #print("Dado de Other2: ", other2.dado)
             new.listaNos.append(other2)
+            new.ePalavra = False
             root = new
 
             print('\nesse eh o new',new.dado,'esse eh o root',root.dado)
-            root.ePalavra = False
             return root
     elif match == len(root.dado):
         #quarto caso: prefixo em comum, sendo esse prefixo o root
@@ -94,9 +81,9 @@ def addNo(root, entrada):
                 for j,i in enumerate(root.listaNos):
                     if i.dado[0] == entrada[match:][0]:
                         flag =True
-                        print('\nRECURSAO',entrada,'  ',entrada[match:])
+                        #print('\nRECURSAO',entrada,'  ',entrada[match:])
                         x=addNo(i,entrada[match:])
-                        print('\nTESTE',x.dado,x.listaNos[0].dado,x.listaNos[1].dado,j,root.listaNos[j].dado)
+                        #print('\nTESTE',x.dado,x.listaNos[0].dado,x.listaNos[1].dado,j,root.listaNos[j].dado)
                         root.listaNos[j]=x
 
             if len(root.listaNos) == 0 or flag == False:
@@ -108,19 +95,28 @@ def addNo(root, entrada):
     return root
 
 def imprimindo(root):
-    print(root.dado)
-    if(len(root.listaNos)>0):
+    if(len(root.listaNos)!=0):
+        print('\n*',root.dado,root.ePalavra)
+        for i in root.listaNos:
+            print('[', end='')
+            print(i.dado, end='')
+            print(']',end='')
         for i in root.listaNos:
             imprimindo(i)
+
+
+def buscando(root, prefixo):
+    if root.dado[0] == prefixo[0]:
+        print('ACHOU')
+        for i in root.listaNos:
+            #if i.ePalavra == True:
+            print(i.pOriginal,i.ePalavra, i.pOriginal)
+            for j in i.listaNos:
+                    #if j.ePalavra == True:
+                print(j.dado, j.ePalavra, j.pOriginal)
+
+
 def main():
-
-    #root = No('canto')
-    #root = addNo(root,'cano')
-    #root = addNo(root,'canario')
-    #root = addNo(root,'cantoria')
-    #root = addNo(root,'carro')
-
-
     arq = sys.argv[1]
     f = open(arq, 'r')
     line = f.readline() #Linha 1 - root
@@ -130,24 +126,12 @@ def main():
         print('trans ', trans)
         root = addNo(root,trans)
 
+    print('\n--------PRINT---------------')
     imprimindo(root)
-    #print(root.dado,root.listaNos[0].dado,root.listaNos[1].dado)
-    #print('qnt tem',len(root.listaNos))
-    #print('\n ROOT',root.dado)
-    #print('listnos[0] ', root.listaNos[0].dado)
-    #print('listnos[1] ', root.listaNos[1].dado)
-    #print('listnos[1] ', root.listaNos[2].dado)
-    #print('listnos FILHO ', root.listaNos[1].listaNos[0].dado)
-    #print('listnos FILHO e root',root.dado, root.listaNos[1].listaNos[0].dado)#.listaNos[1].dado)
-    #print('listnos FILHO ', root.listaNos[1].listaNos[1].listaNos[0].dado)
-    #print('listnos FILHO ', root.listaNos[1].listaNos[2].dado)
+    print('')
 
-
-    #print('\n ROOT-Filho[0]',root.listaNos[0].dado)
-    #print('\n ROOT-Filho[1]',root.listaNos[1].dado)
-    #print('\n ROOT-Filho[1]',root.listaNos[1].listaNos[0])
-    #print('\n ROOT-Filho[2]',root.listaNos[2].dado)
-    #print('\n ROOT-Filho[2]',root.listaNos[2].dado)
+    print('\n-------BUSCANDO------')
+    buscando(root, "c")
 
 if __name__ == "__main__":
     main()
